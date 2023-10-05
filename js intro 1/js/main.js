199a45c8eb794da0266d51270ec320c85839c66d
Robert <Robert@step.rivne>
2023-10-05 18:25:35 +0300

    first commit

---
+++ js intro 1/js/main.js
@@ -0,0 +1,100 @@
+//! типи змінних
+
+var number = 1//глобальна зона видимісьті це застарий варіант
+
+
+let number2 = 'test'//блочна зона видимісьті
+const number3 = 3.14//блочна зона видимісьті
+
+// console.log(number)
+// console.log(number2)
+// console.log(number3)
+
+
+//! прості типи даних
+
+//?number
+const age = 20
+// console.log('Age = ', age)
+// console.log('Age type = ', typeof age)
+
+const value = 3.14
+
+// console.log('value = ', value)
+// console.log('value type = ', typeof value)
+
+
+//?string
+
+const firstName = 'Bill'
+ // console.log('firstName = ', firstName)
+// console.log('firstName type = ', typeof firstName)
+
+
+const any = 'sdfgdsfg 10 sdfgdsf sdfg 1212'
+// console.log('any = ', any)
+// console.log('any type = ', typeof any)
+
+//?boolen
+
+let isActivate = false
+
+// console.log('isActivate = ', isActivate)
+// console.log('isActivate type = ', typeof isActivate)
+
+isActivate = true
+
+// console.log('isActivate = ', isActivate)
+// console.log('isActivate type = ', typeof isActivate)
+
+//?undefaint змінна не визначена
+
+let test
+
+// console.log(test);
+// console.log(typeof test);
+
+test 
+// console.log(test);
+// console.log(typeof test);
+
+
+//? null
+
+const port = null
+// console.log( typeof port);
+
+
+// ==============================================================================================================
+//! матиматичні дії
+
+const a = 5.2, b = 10, c = 2
+
+let result
+
+// result = a + b + c
+// result = a - b
+// result = c * b
+// result = b / c
+// result = a % c
+result = 2 ** 3
+
+console.log(result);
+
+// ====================================================================================================================
+
+
+const hello = 'hello it`s my first app'
+
+// alert(hello)
+
+
+// const num1 = +prompt('Enter number 1')
+// const num2 = +prompt('Enter number 2')
+
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// alert(num1 + num2)
+
